83d45e13e20aff9b0022d23b4a0934ca571dd25d
meanprogrammer <mean.programmer.d@gmail.com>
2015-09-01 17:06:24 +0800

    front ends

--- ChurchMemberProfile.Web/ChurchMemberProfile.Web/app/indexApp.js
+++ ChurchMemberProfile.Web/ChurchMemberProfile.Web/app/indexApp.js
@@ -1,6 +1,7 @@
 ﻿var app = angular.module('vpApp', ['ngRoute', 'ui.bootstrap', 'ui.bootstrap.tpls', 'ngSanitize']).
     config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
         $routeProvider.
-            when('/index', { templateUrl: 'html/indexContent.html', controller: 'IndexCtrl' })
+            when('/index', { templateUrl: 'html/indexContent.html', controller: 'IndexCtrl' }).
+            when('/template', { templateUrl: 'html/createTemplate.html', controller: 'TemplateCtrl' })
         .otherwise({ redirectTo: '/index' });
     }]);
---
+++ ChurchMemberProfile.Web/ChurchMemberProfile.Web/ctrls/template.js
@@ -0,0 +1,26 @@
+﻿angular.module('vpApp').controller('TemplateCtrl', ['$scope', '$modal',
+    function ($scope, $modal) {
+        
+        $scope.openModal = function (id) {
+            var modalInstance = $modal.open({
+                animation: true,
+                templateUrl: 'activityUserModal.html',
+                controller: 'ActivityUserModalInstanceCtrl',
+                size: 'md',
+                resolve: {
+                    modalId: function () {
+                        return id;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function (result) {
+
+            }, function () {
+                $log.info('Modal dismissed at: ' + new Date());
+            });
+        };
+
+
+    }
+]);
