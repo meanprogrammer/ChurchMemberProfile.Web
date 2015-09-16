87c1f783b3100984b0f5e1208481fe1772d029e5
meanprogrammer <mean.programmer.d@gmail.com>
2015-09-16 21:25:55 +0800

    fix 3

--- ChurchMemberProfile.Web/ChurchMemberProfile.Web/app/indexApp.js
+++ ChurchMemberProfile.Web/ChurchMemberProfile.Web/app/indexApp.js
@@ -1,7 +1,12 @@
 ﻿var app = angular.module('vpApp', ['resourceService','ngRoute', 'ui.bootstrap', 'ui.bootstrap.tpls', 'ngSanitize']).
-    config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+    config(['$routeProvider', '$locationProvider', '$httpProvider', function ($routeProvider, $locationProvider, $httpProvider) {
         $routeProvider.
             when('/index', { templateUrl: 'html/indexContent.html', controller: 'IndexCtrl' }).
             when('/template', { templateUrl: 'html/createTemplate.html', controller: 'TemplateCtrl' })
         .otherwise({ redirectTo: '/index' });
+
+        $httpProvider.defaults.useXDomain = true;
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+        $httpProvider.defaults.headers.common["Accept"] = "application/json";
+        $httpProvider.defaults.headers.common["Content-Type"] = "application/json";
     }]);
