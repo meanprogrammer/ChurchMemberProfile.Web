4e5ccc328dc29dd2d8512562fc5270c550d19941
meanprogrammer <mean.programmer.d@gmail.com>
2015-09-16 21:15:21 +0800

    fix

--- ChurchMemberProfile.Web/ChurchMemberProfile.Web/ctrls/index.js
+++ ChurchMemberProfile.Web/ChurchMemberProfile.Web/ctrls/index.js
@@ -1,7 +1,7 @@
 ﻿angular.module('vpApp').controller('IndexCtrl', ['$scope', 'DudzApiService',
     function ($scope, DudzApiService) {
         console.log('test');
-        $scope.init = function () { DudzApiService.get(); };
+        $scope.init = function () { DudzApiService.get(function (data) { console.log(data); }); };
         $scope.init();
     }
 ]);
