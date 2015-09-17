ab0e5c64b655121b429dc4e26071c2b2d540e2ec
meanprogrammer <mean.programmer.d@gmail.com>
2015-09-17 22:01:37 +0800

    try fix

--- ChurchMemberProfile.Web/ChurchMemberProfile.Web/app/indexApp.js
+++ ChurchMemberProfile.Web/ChurchMemberProfile.Web/app/indexApp.js
@@ -7,6 +7,4 @@
 
         $httpProvider.defaults.useXDomain = true;
         delete $httpProvider.defaults.headers.common['X-Requested-With'];
-        $httpProvider.defaults.headers.common["Accept"] = "application/json";
-        $httpProvider.defaults.headers.common["Content-Type"] = "application/json";
     }]);
