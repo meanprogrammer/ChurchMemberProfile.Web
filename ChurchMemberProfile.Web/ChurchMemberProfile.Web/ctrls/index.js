dbb84f08fb12e83a984e86b3c65c8993f2c70401
meanprogrammer <mean.programmer.d@gmail.com>
2015-10-06 17:43:24 +0800

    test 2

--- ChurchMemberProfile.Web/ChurchMemberProfile.Web/ctrls/index.js
+++ ChurchMemberProfile.Web/ChurchMemberProfile.Web/ctrls/index.js
@@ -3,6 +3,7 @@
         console.log('test');
         $scope.init = function () {
             console.log("init() executed!");
+            $scope.init = function () { DudzApiService.get(function (data) { console.log(data); }); };
         };
         $scope.init();
     }
