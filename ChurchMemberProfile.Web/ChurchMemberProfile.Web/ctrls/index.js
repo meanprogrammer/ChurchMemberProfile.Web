907361d497a262da0f3c27bb2f62df92ca19db35
meanprogrammer <mean.programmer.d@gmail.com>
2015-10-06 17:47:11 +0800

    test 3

--- ChurchMemberProfile.Web/ChurchMemberProfile.Web/ctrls/index.js
+++ ChurchMemberProfile.Web/ChurchMemberProfile.Web/ctrls/index.js
@@ -1,10 +1,7 @@
 ﻿angular.module('vpApp').controller('IndexCtrl', ['$scope', 'DudzApiService',
     function ($scope, DudzApiService) {
         console.log('test');
-        $scope.init = function () {
-            console.log("init() executed!");
-            $scope.init = function () { DudzApiService.get(function (data) { console.log(data); }); };
-        };
+        $scope.init = function () { console.log("init() executed!"); DudzApiService.get(function (data) { console.log(data); }); };
         $scope.init();
     }
 ]);
