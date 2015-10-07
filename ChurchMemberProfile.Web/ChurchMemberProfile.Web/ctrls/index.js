e2aa2e2867083e33d241375a1d61cd9fda59d71e
meanprogrammer <mean.programmer.d@gmail.com>
2015-10-07 17:40:31 +0800

    test

--- ChurchMemberProfile.Web/ChurchMemberProfile.Web/ctrls/index.js
+++ ChurchMemberProfile.Web/ChurchMemberProfile.Web/ctrls/index.js
@@ -1,7 +1,7 @@
-﻿angular.module('vpApp').controller('IndexCtrl', ['$scope', 'DudzApiService',
+﻿angular.module('vpApp').controller('IndexCtrl', ['$scope', 'MemberProfileService',
     function ($scope, DudzApiService) {
         console.log('test');
-        $scope.init = function () { console.log("init() executed!"); DudzApiService.get(function (data) { console.log(data); }); };
+        $scope.init = function () { console.log("init() executed!"); MemberProfileService.all.get(function (data) { console.log(data); }); };
         $scope.init();
     }
 ]);
--- ChurchMemberProfile.Web/ChurchMemberProfile.Web/shared/services/resourceService.js
+++ ChurchMemberProfile.Web/ChurchMemberProfile.Web/shared/services/resourceService.js
@@ -4,8 +4,29 @@ angular.module('resourceService', ['ngResource'])
 	   .factory(
 		'DudzApiService',
 		function ($resource) {
-		  		    return $resource('http://discipleshipws.apphb.com/api/memberprofile',
-					{}, {
-					    get: { method: 'GET', isArray: false }
-					});
-		});
+		    return $resource('http://discipleshipws.apphb.com/api/memberprofile',
+            {}, {
+                get: { method: 'GET', isArray: false }
+            });
+		})
+.factory(
+				'MemberProfileService',
+				function ($resource) {
+				    return {
+
+				        one: $resource(
+								'http://discipleshipws.apphb.com/api/memberprofile/:id', {},
+								{
+								    get: {
+								        method: 'GET',
+                                        isArray: false
+								    }
+								}),
+				        all: $resource('http://discipleshipws.apphb.com/api/memberprofile/', {}, {
+				            get: {
+				                method: 'GET',
+                                isArray: true
+				            }
+				        }),
+				    };
+				});
