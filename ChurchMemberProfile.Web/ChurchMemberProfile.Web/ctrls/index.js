56e8d3c39db62fe9bd913bcfd45dc1c3cecdabe4
meanprogrammer <mean.programmer.d@gmail.com>
2015-10-07 17:44:28 +0800

    test fail

--- ChurchMemberProfile.Web/ChurchMemberProfile.Web/ctrls/index.js
+++ ChurchMemberProfile.Web/ChurchMemberProfile.Web/ctrls/index.js
@@ -1,5 +1,5 @@
 ﻿angular.module('vpApp').controller('IndexCtrl', ['$scope', 'MemberProfileService',
-    function ($scope, DudzApiService) {
+    function ($scope, MemberProfileService) {
         console.log('test');
         $scope.init = function () { console.log("init() executed!"); MemberProfileService.all.get(function (data) { console.log(data); }); };
         $scope.init();
